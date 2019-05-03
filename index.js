7c930ca572e4a8ba4cb17d033b5cb2c5a97808e6
Caroline Ninganga <carolninganga@gmail.com>
2019-05-03 17:51:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b) {
+  return a + b;
+}
+
+add(4,6);
+
+function subtract(a,b) {
+  return a - b;
+}
+subtract(8,4);
+
+function multiply(a,b) {
+  return a * b;
+}
+multiply(8,4);
+
+function divide(a,b) {
+  return a / b;
+}
+divide(8,4);
+
+
+function inc(n) {
+  var n = n + 1;
+  return n;
+}
+inc(7);
+
+function dec(n) {
+  var n = n -= 1;
+  return n;
+}
+dec(7);
+
+function makeInt(n) {
+  return n;
+}
+
+function makeInt(n) {
+  parseInt('0', 10);
+  return n;
+}
+
+makeInt();
+
+function preserveDecimal(n) {
+  parseFloat('90.9907');
+  return n;
+}
+preserveDecimal();
